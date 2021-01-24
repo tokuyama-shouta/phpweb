d64b238fa3cb208aa1953d001246b0f73da9550a
tokuyama shouta <toku1031ts@gmail.com>
2021-01-24 16:49:28 +0900

    dbから内容を取り出す

--- dbc.php
+++ dbc.php
@@ -8,7 +8,14 @@
       $dbh = new PDO($dsn,$user,$pass,[
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       ]);
-      echo '接続成功';
+      // echo '接続成功';
+      // 1 SQLの準備
+      $sql = 'SELECT * FROM blog';
+      // 2 SQLの実行
+      $stmt = $dbh->query($sql);
+      // 3 SQLの結果を受け取る
+      $result = $stmt->fetchall(PDO::FETCH_ASSOC);
+      // var_dump($result);
       $dbh = null;
   } catch(PDOException $e) {
       echo '接続失敗'. $e->getMessage();
@@ -16,6 +23,33 @@
   };
 
 
-  var_dump($dbh);
+  // var_dump($dbh);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ブログ一覧</title>
+</head>
+<body>
+  <h2>ブログ一覧</h2>
+  <table>
+    <tr>
+      <th>No</th>
+      <th>タイトル</th>
+      <th>カテゴリ</th>
+    </tr>
+    <?php foreach($result as $column): ?>
+    <tr>
+      <td><?php echo $column['id']?></td>
+      <td><?php echo $column['title']?></td>
+      <td><?php echo $column['category']?></td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+  
+</body>
+</html>
