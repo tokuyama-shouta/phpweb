b5b62ac57a7b3d053ebf7422779df7d0a2494e75
tokuyama shouta <toku1031ts@gmail.com>
2021-01-31 18:50:50 +0900

    if

--- blog.php
+++ blog.php
@@ -2,10 +2,18 @@
 
 $blog = $_POST;
 
-foreach($blog as $key => $value) {
-  echo '<pre>';
-  echo $key . ':' . htmlspecialchars($value,ENT_QUOTES,'UTF-8');
-  echo '</pre>';
+if ($blog['publish_status'] === 'publish') {
+  
+  foreach($blog as $key => $value) {
+    echo '<pre>';
+    echo $key . ':' . htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+    echo '</pre>';
+  }
+} elseif ($blog['publish_status'] === 'un_publish') {
+  echo '公開記事がありません。';
+} else {
+  echo '記事がありません。';
 }
+
 
 ?>
