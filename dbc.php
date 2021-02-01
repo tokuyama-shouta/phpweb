37b127b19095fdd51dd6cc9d693d7ecd75548098
tokuyama shouta <toku1031ts@gmail.com>
2021-02-01 22:24:52 +0900

    php mysqlDb

---
+++ dbc.php
@@ -0,0 +1,22 @@
+<?php
+
+$dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
+$user = 'blog_user';
+$pass = 'ts1031';
+
+try {
+  $dbh = new PDO($dsn,$user,$pass,[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  ]);
+    echo '接続成功';
+    $dbh = null;
+} catch(PDOException $e) {
+  echo '接続失敗'. $e->getMessage();
+  exit();
+};
+
+
+var_dump($dbh);
+
+
+?>
