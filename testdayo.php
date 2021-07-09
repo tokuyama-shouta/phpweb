edc08a1a1d58ef5d40eb2d44fed4c43d4f569c74
tokuyama shouta <toku1031ts@gmail.com>
2021-07-09 22:36:05 +0900

    php 配列

--- testdayo.php
+++ testdayo.php
@@ -8,24 +8,50 @@ $post_at = "2020/07/08";
 $tag = ['PHP', 'プログラミング'];
 $status = true;
 //定数
-const ID = 1;
-echo ID;
-echo $title;
-echo'<br>';
-echo $contents;
-echo'<br>';
-echo $post_at;
-echo'<br>';
-print_r($tag);
+// const ID = 1;
+// echo ID;
+// echo $title;
+// echo'<br>';
+// echo $contents;
+// echo'<br>';
+// echo $post_at;
+// echo'<br>';
+// print_r($tag);
 
-//データ型
-var_dump(ID);
-//""と''
-echo'<br>';
-echo "タイトル名:$title";
-echo'<br>';
-echo 'タイトル名:$title';
+// //データ型
+// var_dump(ID);
+// //""と''
+// echo'<br>';
+// echo "タイトル名:$title";
+// echo'<br>';
+// echo 'タイトル名:$title';
 
+$blog1 = array(
+  'id' => ID,
+  'title' => $title,
+  'contents' => $contents,
+  'posta_at' => $post_at,
+  'tag' => $tag,
+  'status' => $status
+);
+
+echo $blog1['title'];
+
+$blog2 = [
+  'id2' => ID2,
+  'title2' => $title2,
+  'contents2' => $contents2,
+  'posta_at2' => $post_at2,
+  'tag2' => $tag2,
+  'status2' => $status2
+
+];
+
+$blog = [$blog1,$blog2];
+  
+echo '<pre>';
+var_dump($blog2);
+echo '</pre>';
 
 
 
