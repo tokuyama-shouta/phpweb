f4f0fc49e21609c5bb672ca6d79561c2447de1e1
tokuyama shouta <toku1031ts@gmail.com>
2021-07-13 23:39:25 +0900

    php if

--- blogdayo.php
+++ blogdayo.php
@@ -2,10 +2,20 @@
 
 //$blog = $_GET;
 $blog = $_POST;
-foreach($blog as $key => $value) {
-  echo '<pre>';
-  echo $key . ':' . htmlspecialchars($value,ENT_QUOTES,'UTF-8') . $value;
-  echo '</pre>';
+
+if($blog['publish_status'] === 'publish') {
+  foreach($blog as $key => $value) {
+    echo '<pre>';
+    echo $key . ':' . htmlspecialchars($value,ENT_QUOTES,'UTF-8') . $value;
+    echo '</pre>';
+  }
+}elseif($blog['publish_status'] === 'un_publish'){
+  echo '公開中の記事はありません。';
+}else{
+  echo '記事がありません。';
 }
+
+
+
 
 ?>
