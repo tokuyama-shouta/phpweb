58cf387a5e8c67c2405163489c2badba3b5e3c9a
tokuyama shouta <toku1031ts@gmail.com>
2021-07-19 23:14:45 +0900

    ブログ一覧

--- dbcdayo.php
+++ dbcdayo.php
@@ -8,7 +8,14 @@ try{
   $dbh = new PDO($dsn,$user,$pass,[
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
   ]);
-  echo '接続成功';
+  //echo '接続成功';
+  //sql準備
+  $sql = 'SELECT * FROM blog';
+  //sqlの実行
+  $stmt = $dbh->query($sql);
+  //sqlの結果を受け取る
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  //var_dump($result);
   $dbh = null;
 
 } catch(PDOException $e) {
@@ -17,3 +24,30 @@ try{
 };
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ブログ一覧</title>
+</head>
+<body>
+  <h2>ブログ一覧</h2>
+  <table>
+    <tr>
+      <th>No</th>
+      <th>タイトル</th>
+      <th>カテゴリ</th>
+    </tr>
+    <?php foreach($result as $column): ?>
+    <tr>
+      <td><?php echo $column['id'] ?></td>
+      <td><?php echo $column['title'] ?></td>
+      <td><?php echo $column['category'] ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+</html>
