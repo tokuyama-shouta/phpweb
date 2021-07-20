25f1cf1fe7b93047b8c302cb8eb4ba4705335b22
tokuyama shouta <toku1031ts@gmail.com>
2021-07-20 20:06:25 +0900

    関数

--- dbcdayo.php
+++ dbcdayo.php
@@ -1,27 +1,64 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
-$user = 'bogdayo_user';
-$pass = 'ts1031205';
+// 関数1つに1つの機能のみを持たせる
+// 1.データベース接続
+// 2.データを取得
+// 3.カテゴリー名を表示する
 
-try{
-  $dbh = new PDO($dsn,$user,$pass,[
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  ]);
-  //echo '接続成功';
-  //sql準備
-  $sql = 'SELECT * FROM blog';
-  //sqlの実行
-  $stmt = $dbh->query($sql);
-  //sqlの結果を受け取る
-  $result = $stmt->fetch(PDO::FETCH_ASSOC);
-  //var_dump($result);
-  $dbh = null;
+// 1.データベース接続
+// 引数 なし
+// 返り値：接続結果を返す
+function bdConnect() {
 
-} catch(PDOException $e) {
-  echo '接続失敗'.$e->getMessage();
-  exit();
-};
+  $dsn = 'mysql:host=localhost;dbname=blog_app;charset=utf8';
+  $user = 'bogdayo_user';
+  $pass = 'ts1031205';
+
+    try{
+      $dbh = new PDO($dsn,$user,$pass,[
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      ]);
+    
+    } catch(PDOException $e) {
+      echo '接続失敗'.$e->getMessage();
+      exit();
+    };
+    return $dbh;
+  }
+    //echo '接続成功';
+    
+
+  // 2.データを取得
+  // 引数なし
+  // 返り値：取得したデータを返す
+  function getAllBlog() {
+    $dbh = dbConnect();
+    //sql準備
+    $sql = 'SELECT * FROM blog';
+    //sqlの実行
+    $stmt = $dbh->query($sql);
+    //sqlの結果を受け取る
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    return $result;
+    $dbh = null;
+  }
+
+  // 2.カテゴリー名を表示
+  // 引数 数字
+  // 返り値：カテゴリーの文字列
+  function setCategoryName($category) {
+    if($category === '1'){
+      return 'ブログ';
+    }elseif($category === '2'){
+      return '日常';
+    }else{
+      return 'その他';
+    }
+  }
+
+
+
+
 
 ?>
 
@@ -45,7 +82,7 @@ try{
     <tr>
       <td><?php echo $column['id'] ?></td>
       <td><?php echo $column['title'] ?></td>
-      <td><?php echo $column['category'] ?></td>
+      <td><?php echo setCategoryName($column['category']) ?></td>
     </tr>
     <?php endforeach; ?>
   </table>
